1b8d35374a485b525674106f80892e69f453e736
Delilah Chadic <delilahchadic@gmail.com>
2026-03-13 16:52:08 -0700

    add basic colors

---
+++ src/palette.h
@@ -0,0 +1,30 @@
+// palette.h
+#ifndef PALETTE_H
+#define PALETTE_H
+
+#include "raylib.h"
+
+// The "Highland" Sun-Bleach Colors
+#define COLOR_ASPHALT     (Color){ 45, 45, 48, 255 }
+#define COLOR_DUSTY_ROAD  (Color){ 214, 190, 150, 255 }
+#define COLOR_BEAVIS_SHIRT (Color){ 51, 153, 255, 255 }
+
+// The "Bebop" Night Colors
+#define COLOR_VHS_BLUE    (Color){ 0, 20, 100, 255 }
+#define COLOR_NEON_CRIMSON (Color){ 220, 20, 60, 255 }
+#define COLOR_TAROT_GOLD   (Color){ 255, 215, 0, 255 }
+// The "Americana" Warm Tones (Faded, dusty, sun-bleached)
+#define COLOR_DUSTY_ROSE    (Color){ 194, 118, 128, 255 }
+#define COLOR_DUSTY_SALMON  (Color){ 214, 140, 121, 255 }
+#define COLOR_DUSTY_CORAL   (Color){ 205, 126, 107, 255 }
+#define COLOR_MUSTARD       (Color){ 203, 153, 50, 255 }
+
+// The "Bebop/Space" Cool Tones (Deep, punchy, technical)
+#define COLOR_PRUSSIAN_BLUE (Color){ 0, 49, 83, 255 }
+#define COLOR_ULTRAMARINE   (Color){ 18, 10, 143, 255 }
+#define COLOR_JADE          (Color){ 0, 168, 107, 255 }
+
+// A bonus "Slacker" background color (The color of a dirty Texas sky)
+#define COLOR_TEXAS_HAZE    (Color){ 220, 210, 190, 255 }
+
+#endif
