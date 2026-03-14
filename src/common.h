481d7e64c5c3e47df477070a3eeb47d5dfcff8de
Delilah Chadic <delilahchadic@gmail.com>
2026-03-13 21:49:10 -0700

    move item definition to common location

---
+++ src/common.h
@@ -0,0 +1,18 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+typedef enum{
+  ITEM_VHS_TAPE,
+  ITEM_TAROT_CARD,
+  ITEM_MISC,
+  ITEM_KEY_ITEM,
+  ITEM_MINERAL
+} ItemType;
+typedef struct {
+    int id;
+    char name[32];
+    char description[128];
+    // Co lor rarityColor;
+    ItemType type; // For that "Bebop" flair
+} ItemDefinition;
+#endif
--- src/inventory.h
+++ src/inventory.h
@@ -1,35 +1,8 @@
 #ifndef INVENTORY_H
 #define INVENTORY_H
 #include "palette.h"
+#include "common.h"
 #define MAX_INVENTORY_SLOTS 20
-
-typedef enum{
-  ITEM_VHS_TAPE,
-  ITEM_TAROT_CARD,
-  ITEM_MISC,
-  ITEM_KEY_ITEM,
-  ITEM_MINERAL
-} ItemType;
-typedef struct {
-    int id;
-    char name[32];
-    char description[128];
-    Color rarityColor;
-    ItemType type; // For that "Bebop" flair
-} ItemDefinition;
-
-// The "Item Database" - The master list of everything in the world
-static const ItemDefinition ITEM_DB[] = {
-    { 0, "Empty Slot", "", GRAY ,ITEM_KEY_ITEM},
-    { 1, "Cracked VHS", "Labeled 'Highland High '94'. It smells like old carpet.", COLOR_DUSTY_ROSE,ITEM_KEY_ITEM },
-    { 2, "The Tower Card", "A Tarot card found in a storm drain. It's soggy.", COLOR_TAROT_GOLD ,ITEM_TAROT_CARD},
-    { 3, "Rusty Tab", "From a soda can discontinued in the 80s.", COLOR_MUSTARD,  ITEM_MISC},
-    { 4, "Lapiz Lazuli", "blue", COLOR_ULTRAMARINE, ITEM_MINERAL}
-};
-
-
-
-
 
 typedef struct Inventory{
   int itemIds[MAX_INVENTORY_SLOTS];
