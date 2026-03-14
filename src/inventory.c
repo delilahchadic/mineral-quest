374ce3997731bdce813407bb4401316f038d8216
Delilah Chadic <delilahchadic@gmail.com>
2026-03-13 18:22:05 -0700

    update variable name

--- src/inventory.c
+++ src/inventory.c
@@ -2,7 +2,7 @@
 
 void InitInventory(Inventory* inventory){
   inventory->count =0;
-  inventory->selectedSlot = 0;
+  inventory->selected = 0;
   for(int i = 0; i < 20; i++) {
     inventory->itemIds[i] = 0;
   }
