16f2457b7b501a816841cd40ed0c413c1e28cfc2
Delilah Chadic <delilahchadic@gmail.com>
2026-03-17 19:47:39 -0700

    remove erroneus white space

--- src/engine/main.c
+++ src/engine/main.c
@@ -11,11 +11,11 @@ int main(void) {
   while (!WindowShouldClose()) {
     // --- 1. Update Logic ---
     UpdateScene(&gamestate);
-      // --- 2. Drawing ---
-      BeginDrawing();
-        ClearBackground(COLOR_PULP_PAPER);
-        DrawScreen(&gamestate);
-      EndDrawing();
+    // --- 2. Drawing ---
+    BeginDrawing();
+      ClearBackground(COLOR_PULP_PAPER);
+      DrawScreen(&gamestate);
+    EndDrawing();
   }
   CloseGame(&gamestate);
   return 0;
