6bcaeff2ac1996ddffe5b8b87f649070655aef97
Delilah Chadic <delilahchadic@gmail.com>
2026-04-13 12:51:18 -0700

    move tile information into register

--- src/engine/common.h
+++ src/engine/common.h
@@ -34,4 +34,14 @@ typedef struct Plant{
   int hitboxwidth;
 } Plant;
 
+typedef enum { TILE_WATER,TILE_GRASS ,TILE_SAND, TILE_DIRT,TILE_STONE, TILE_ROAD} TileType;
+
+
+typedef struct TileDefinition{
+    int id;             // 0 = Sand, 1 = Asphalt, 2 = Magnetic Pit
+    bool is_blocking;   //
+    float friction;     // 1.0 = Normal, 0.2 = Ice/Oil, 1.5 = Deep Sand
+    int footstep_sfx;
+    Color color;// sound??
+} TileDefinition;
 #endif
