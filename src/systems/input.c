9517a8badc04c76bfcd03e193743dc8660a13e3c
Delilah Chadic <delilahchadic@gmail.com>
2026-04-13 19:25:04 -0700

    make inventory and dialog both on key press not down

--- src/systems/input.c
+++ src/systems/input.c
@@ -8,8 +8,8 @@ Input CaptureInput(){
   if (IsKeyDown(KEY_D)) i.dir.x += 1;
   if (IsKeyDown(KEY_S)) i.dir.y += 1;
   i.jump = IsKeyDown(KEY_SPACE);
-  i.inventory = IsKeyDown(KEY_I);
-  i.dialog = IsKeyDown(KEY_E);
+  i.inventory = IsKeyPressed(KEY_I);
+  i.dialog = IsKeyPressed(KEY_E);
 
   return i;
 }
