02a5f9d07ebd2c50054e8fd994074f8b1732c847
Delilah Chadic <delilahchadic@gmail.com>
2026-04-22 14:51:22 -0700

    define editsession in types_engine.h

--- src/defs/types_engine.h
+++ src/defs/types_engine.h
@@ -7,9 +7,10 @@
 #include "defs/types_env.h"
 #include "defs/types_systems.h"
 
-typedef enum GameScreen { LOGO = 0, TITLE, GAMEPLAY,MENU } GameScreen;
+typedef enum GameScreen { LOGO = 0, TITLE, GAMEPLAY,MENU, EDIT_SCREEN } GameScreen;
 
 typedef enum PlayState { ADVENTURE = 0, INVENTORY, TALKING, ITEM } PlayState;
+typedef enum EditState {EDITOR_MENU = 0, EDITOR, EDITOR_PROMPT}EditState;
 
 typedef struct PlaySession{
   Player player;
@@ -20,11 +21,18 @@ typedef struct PlaySession{
   char pendingItemName[100];
 } PlaySession;
 
+typedef struct EditSession{
+  Map map;
+  SystemMenu menu;
+  EditorForm form;
+  EditState state;
+} EditSession;
 
 typedef struct Gamestate{
   GameScreen screen;
   int framesCounter;
   SystemMenu main_menu;
+  EditSession edit_session;
   PlaySession session;
 } Gamestate;
 
