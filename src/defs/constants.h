1e37bdcb40a9dd6cc3f729d7346505cd19b03f53
Delilah Chadic <delilahchadic@gmail.com>
2026-04-24 17:23:57 -0700

    create a header for constants to share throughout the program

---
+++ src/defs/constants.h
@@ -0,0 +1,8 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 800
+#define TARGET_FPS 60
+#define MAX_SLOTS 20
+const static float DRAG_THRESHOLD = 4.0f;
+#endif
