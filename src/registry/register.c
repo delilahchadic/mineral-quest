f75ba4b912e91879a54f3725d77286aabc1e53cc
Delilah Chadic <delilahchadic@gmail.com>
2026-04-30 00:37:05 -0700

    add more tile types

--- src/registry/register.c
+++ src/registry/register.c
@@ -1,6 +1,7 @@
 #include "registry/register.h"
 #include "defs/types_engine.h"
 #include "defs/types_entities.h"
+#include "engine/palette.h"
 
 ItemDefinition ITEM_REGISTRY[100] = {0};
 Message DIALOG_REGISTRY[500] = {0};
@@ -10,7 +11,7 @@ Plant PLANT_REGISTRY[100] = {0};
 Player GLOBAL_PLAYER;
 Player* PLAYER;
 static int item_count = 0;
-TileDefinition TILE_REGISTRY[8] = {
+TileDefinition TILE_REGISTRY[20] = {
   { TILE_WATER, true,  0.5f, 101, COLOR_CERULEAN_DUSTY,"Water"},
   { TILE_GRASS, false,  1.0f, 102, COLOR_DUSTY_SAP,"Grass"},
   { TILE_SAND,  false, 0.4f, 103, COLOR_PALE_SAND,"Sand"},
@@ -18,7 +19,19 @@ TileDefinition TILE_REGISTRY[8] = {
   { TILE_STONE, false, 0.9f, 105, COLOR_TEXAS_HAZE,"Stone"},
   { TILE_ROAD,  false, 1.0f, 106, COLOR_ASPHALT ,"Road"},
   {TILE_TERRA_PALE,false,1.0f,107,COLOR_TERRA_PALE, "Terra Pale"},
-  {TILE_SILT, false, 1.0f, 108, COLOR_OCHRE_SILT,"Ochre Silt"}
+  {TILE_SILT, false, 1.0f, 108, COLOR_OCHRE_SILT,"Ochre Silt"},
+  { TILE_MOON_STONE, false,  1.0f, 102, COLOR_MOONGLOW,"Moon Glow"},
+  { TILE_PURPLE_OCHRE,  false, 0.4f, 103, COLOR_PURPLE_OCHRE,"Purple Ochre"},
+  { TILE_BUFF_TITANIUM,  false, 0.8f, 104, COLOR_BUFF_TITANIUM,"Buff Titanium"},
+  { TILE_POTTERS_PINK, false, 0.9f, 105, COLOR_POTTERS_PINK,"Potters Pink"},
+  { TILE_ROSE,  false, 1.0f, 106, COLOR_DUSTY_ROSE ,"Rose"},
+  { TILE_CELADON,false,1.0f,107,COLOR_CELADON, "Celadon"},
+  { TILE_MAY_GREEN, false, 1.0f, 108, COLOR_GREEN_GOLD,"Green Gold"},
+  { TILE_INDANTHRONE, false, 1.0f, 108, COLOR_INDANTHRONE_BLUE,"Indanthrone"},
+  { TILE_SAGE,  false, 0.8f, 104, COLOR_SAGE_BRUSH,"Sage"},
+  { TILE_CHROME_YELLOW, false, 0.9f, 105, COLOR_DUSTY_CORAL,"Coral"},
+  { TILE_19,  false, 1.0f, 106, COLOR_WITHERED_LILAC ,"Wistful"},
+  { TILE_20,false,1.0f,107,COLOR_CERULEAN_BERYL, "Beryl"}
 };
 
 char* GetName(EntityType type, int id){
