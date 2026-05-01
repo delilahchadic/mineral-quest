08a85e245ba1f3598687bc6ddaf3520d02a073d4
Delilah Chadic <delilahchadic@gmail.com>
2026-05-01 11:06:23 -0700

    minor white space

--- src/editor/entity_drawer.h
+++ src/editor/entity_drawer.h
@@ -1,12 +1,15 @@
 #ifndef ENTITY_DRAWER_H
 #define ENTITY_DRAWER_H
 
+#include "raylib.h"
+
 #include "defs/types_ui.h"
 #include "defs/types_env.h"
 #include "defs/types_core.h"
 #include "defs/types_systems.h"
-#include "raylib.h"
+
 void InitEnitityDrawer(EntityDrawer* drawer, int startX, int startY);
 bool UpdateEnitityDrawer(EntityDrawer* drawer, Map* map, Input* input,Camera2D* camera);
 void DrawEnitityDrawer(EntityDrawer* drawer);
+
 #endif
