ba74b5da624f1df8dd9cb209b48c0d86028fc6be
Delilah Chadic <delilahchadic@gmail.com>
2026-05-02 21:56:05 -0700

    remove minor whitespace

--- src/systems/script_manager.c
+++ src/systems/script_manager.c
@@ -19,18 +19,18 @@ void DrawMessage(ScriptManager* manager){
     return;
   }
   DrawDialog(manager->active_messsage->character_name, manager->active_messsage->text);
-  
+
 }
 
 void UpdateScriptManager(ScriptManager* manager, Input* input){
   if (manager->active && (input->buttons_pressed & INTERACT_PRESSED)) {
-    // For now, let's just close it. 
+    // For now, let's just close it.
     // Later, this will call Advance_Dialogue to check for nextId
     if(manager->active_messsage->nextid != -1){
       SetActiveMessage(manager, manager->active_messsage->nextid);
       return;
     }
     manager->active = false;
-    manager->active_messsage = NULL; 
+    manager->active_messsage = NULL;
   }
 }
