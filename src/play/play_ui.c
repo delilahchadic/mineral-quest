ab46f4c46749294a04a70b0c6809aefd55fffcc7
Delilah Chadic <delilahchadic@gmail.com>
2026-09-08 22:03:39 -0700

    stats and gear update

--- src/play/play_ui.c
+++ src/play/play_ui.c
@@ -6,12 +6,15 @@
 #include "defs/types_entities.h"
 #include "defs/types_minerals.h"
 #include "defs/types_systems.h"
+#include "engine/palette.h"
 #include "environment/map.h"
 #include "environment/map_ui.h"
 #include "raylib.h"
 #include "registry/mineral_register.h"
 #include "play/play_inventory.h"
+#include "registry/register.h"
 #include "ui/dialog_box.h"
+#include "ui/equip_menu.h"
 #include "systems/script_manager.h"
 
 void AdjustCamera(PlaySession* session, bool dialog, float dt){
@@ -46,12 +49,44 @@ void AdjustCamera(PlaySession* session, bool dialog, float dt){
   }
 }
 
+void DrawStatsMenu(PlaySession* session){
+      // 1. Background - The Aged Paper
+      ClearBackground(COLOR_CERULEAN_COBALT);
+
+      // Dynamic horizontal line based on screen width
+      int margin = 50;
+      int uiWidth = SCREEN_WIDTH - (margin * 2);
+
+      // 2. Title - The "Ink" look
+      DrawText("Stats", margin, 40, 30, COLOR_BONE_WHITE);
+      DrawRectangle(50, 80, uiWidth, 2, COLOR_BONE_WHITE); // A simple line
+
+      DrawText("Base", 250,90,15,COLOR_BONE_WHITE);
+      DrawText("Current", 450,90,15,COLOR_BONE_WHITE);
+      for(int i = 0;i< STAT_COUNT;i++){
+          char base[5],current[5];
+          sprintf(base, "%d", session->map.player->stats->base[i]);
+          sprintf(current,"%d",session->map.player->stats->current[i]);
+          // sprintf(base, "%d", session->player.stats.base[i]);
+                    // sprintf(current, "%d", session->player.stats.current[i]);
+          DrawText(STATS_NAMES[i], margin, 120 + (i * 30), 15, COLOR_BONE_WHITE);
+          DrawText(base,250, 120 + (i * 30), 20, COLOR_BONE_WHITE);
+          DrawText(current,450, 120 + (i * 30), 20, COLOR_BONE_WHITE);
+      }
+}
+
+
 
 void DrawPlaySession(PlaySession* session){
-  if(session->state == INVENTORY){
+
+    if(session->state==EQUIPMENT_MENU){
+        DrawEquipmentMenu(session,&session->equip_menu);
+    }else if(session->state == STATS_MENU){
+      DrawStatsMenu(session);
+  }else if(session->state == INVENTORY){
     DrawInventory(&session->menu);
   }else if(session->state==MINERAL_INVENTORY){
-    DrawMineralInventory(&session->player);
+    DrawMineralInventory(session->player);
   }else if(session->state == LEVEL_INVENTORY){
     DrawLevelInventory();
   }else {
