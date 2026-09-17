34b3f6e7c794b0aa08dc2932a2abf32737ff1442
Delilah Chadic <delilahchadic@gmail.com>
2026-09-17 09:08:21 -0700

    change launch button

--- src/ui/stats_menu.c
+++ src/ui/stats_menu.c
@@ -6,7 +6,7 @@
 #include "registry/register.h"
 
 void UpdateStatsMenu(PlaySession* session, Input* input){
-  if(input->buttons_pressed & SHIFT_DOWN){
+  if(input->buttons_pressed & SHIFT_PRESSED){
     session->state = ADVENTURE;
   }
 }
